4552bb5b9ac9213fce3c353f2a062eae7625988d
Arjunexe <arrjunvm12345@gmail.com>
2024-09-18 14:35:04 +0530

    hi

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const start=function(){
     connectDB(process.env.MONGO_URL)
   }catch(err){
    // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-    console.log(err);
+    console.log('Failed to connect to DB',err);
   }
 }
 
