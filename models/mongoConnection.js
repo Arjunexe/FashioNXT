219cde6e7d496608c5bd27a766800a6fa623bcaa
Gokul Krishna <119096676+am-goku@users.noreply.github.com>
2025-11-25 12:13:14 +0530

    Update mongoConnection.js

--- models/mongoConnection.js
+++ models/mongoConnection.js
@@ -1,11 +1,22 @@
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = async (url) => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    return console.log("Database connected!");
-  } catch (err) {
-    return console.log(err);
-  }
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+/* This code exports a function named `connect` that connects to a MongoDB database using Mongoose. The
+function uses the `mongoose.connect()` method to establish a connection to the database with the
+specified URL and options. If the connection is successful, it logs a message to the console. If
+there is an error, it logs an error message to the console. */
+module.exports.connect = function(){
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+  });
 }
-module.exports = connectDB
